06bb4a8f8c248da79664e86722cec5b861cc099e
kagaririn <kagaririn777@gmail.com>
2025-06-21 23:32:46 +0600

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/portfolio-web' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio-web/' : '',
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
   },
 }
 
